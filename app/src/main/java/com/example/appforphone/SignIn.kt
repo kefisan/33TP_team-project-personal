6bc72a0be311dd7aae5d98d8cfe5afdaa07ef6bd
PhantomAvocado <roma.kot1000@gmail.com>
2024-11-24 14:13:03 +0100

    Adding listener to the button

--- app/src/main/java/com/example/appforphone/SignIn.kt
+++ app/src/main/java/com/example/appforphone/SignIn.kt
@@ -3,11 +3,16 @@ package com.example.appforphone
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +31,24 @@ class SignIn : AppCompatActivity() {
 
             val goToRegisterIntent = Intent(this, RegisterNow::class.java)
             startActivity(goToRegisterIntent)
+        }
+
+        val loginButton = findViewById<Button>(R.id.login_button)
+        val passwordEditText = findViewById<TextInputEditText>(R.id.passwordText)
+        val emailEditText = findViewById<TextInputEditText>(R.id.usernameText)
+        val credentialsManager = CredentialsManager()
+        val loginErrorPopup = Snackbar.make(loginButton,"Wrong email or password",10000)
+
+        loginButton.setOnClickListener{
+            Log.d("Credentials","Login button pressed")
+
+            if(!credentialsManager.isPasswordValid(passwordEditText.text.toString()) && !credentialsManager.isEmailValid(emailEditText.text.toString())) {
+                loginButton.setError("Wrong password or email!")
+                loginErrorPopup.show()
+            }
+            else{
+
+            }
         }
     }
 }
