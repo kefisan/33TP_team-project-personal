ed384055b6368de6343e8b9fc83cfffb2efdaf37
PhantomAvocado <roma.kot1000@gmail.com>
2024-11-24 15:11:31 +0100

    Adding listener to the button with checks

--- app/src/main/java/com/example/appforphone/CredentialsManager.kt
+++ app/src/main/java/com/example/appforphone/CredentialsManager.kt
@@ -1,7 +1,28 @@
 package com.example.appforphone
 
+import java.util.regex.Pattern
+
 class CredentialsManager {
+    //Pattern for the email(stolen from Google)
+    val EMAIL_ADDRESS_PATTERN: Pattern = Pattern.compile(
+        "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                "\\@" +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                "(" +
+                "\\." +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                ")+"
+    )
+
     fun isEmailValid(mail: String): Boolean{
-        return mail.isEmpty();
+        return EMAIL_ADDRESS_PATTERN.matcher(mail).matches()
+    }
+
+
+
+    fun isPasswordValid(pass: String):Boolean{
+        if(pass.length>=8 && pass.count(Char::isDigit)>0 && pass.any {it in "!,+^-_"})
+            return true
+        return false
     }
 }
