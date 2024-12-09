ef0142810021b10d26999f29ed9d21d8d59633a5
PhantomAvocado <roma.kot1000@gmail.com>
2024-12-09 13:32:47 +0100

    Added a test activity with fragments, removed unused folder

---
+++ app/src/main/java/com/example/appforphone/FragmentA.kt
@@ -0,0 +1,14 @@
+package com.example.appforphone
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FragmentA : Fragment(R.layout.fragment_a) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
